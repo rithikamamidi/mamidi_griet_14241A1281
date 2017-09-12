dfd0190b2714467e09cb16c79cdad1e5a8736bba
rithikamamidi <31843257+rithikamamidi@users.noreply.github.com>
2017-09-12 14:06:08 +0530

    try14

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -112,7 +112,7 @@ public class StudentGroup implements StudentArrayOperation {
                     students[i]=students[i-1];
                      }
                   students[index]=student;
-                  students[l+1]=null;
+                  //students[l+1]=null;
                  }
 // Add your implementation here
 	}
